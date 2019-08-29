195a57ffeebbaa631f4af31b59fb88f30999aa6c
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-08-29 03:49:27 -0300

    get github users from github api

---
+++ src/app/controllers/GithubController.js
@@ -0,0 +1,17 @@
+import githubApi from "../../lib/githubApi.js";
+
+class GithubController {
+  async index(req, res) {
+    try {
+      const { data } = await githubApi.get("/users", { params: req.params });
+
+      return res.status(200).send(data);
+    } catch (err) {
+      return res
+        .status(400)
+        .send({ error: "Erro ao buscar usuários pela api do github" });
+    }
+  }
+}
+
+export default new GithubController();
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 
 import auth from "./app/middlewares/auth";
+
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
+import GithubController from "./app/controllers/GithubController";
 
 import validateUserStore from "./app/validators/UserStore";
 import validateUserUpdate from "./app/validators/UserUpdate";
@@ -14,5 +16,7 @@ routes.post("/users", validateUserStore, UserController.store);
 routes.post("/sessions", validateSessionStore, SessionController.store);
 
 routes.put("/users", auth, validateUserUpdate, UserController.update);
+
+routes.get("/admin/github/users", GithubController.index);
 
 export default routes;
