604c1324a607b1c71fd759e5423b064c8ae00212
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-08-29 00:45:27 -0300

    sessions

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 
+import auth from "./app/middlewares/auth";
 import UserController from "./app/controllers/UserController";
+import SessionController from "./app/controllers/SessionController";
 
 const routes = new Router();
 
 routes.post("/users", UserController.store);
+routes.post("/sessions", SessionController.store);
+
+routes.put("/users", auth, UserController.update);
 
 export default routes;
