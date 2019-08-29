6f4da6a2f90158f322bcfb2dccff731d86286482
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-08-29 00:51:48 -0300

    session validator

--- src/routes.js
+++ src/routes.js
@@ -6,11 +6,12 @@ import SessionController from "./app/controllers/SessionController";
 
 import validateUserStore from "./app/validators/UserStore";
 import validateUserUpdate from "./app/validators/UserUpdate";
+import validateSessionStore from "./app/validators/SessionStore";
 
 const routes = new Router();
 
 routes.post("/users", validateUserStore, UserController.store);
-routes.post("/sessions", SessionController.store);
+routes.post("/sessions", validateSessionStore, SessionController.store);
 
 routes.put("/users", auth, validateUserUpdate, UserController.update);
 
