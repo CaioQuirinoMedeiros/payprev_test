0195de8b0f255724aa24d214d5eab5fbad7fe6ba
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-08-29 00:50:19 -0300

    User validators

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,14 @@ import auth from "./app/middlewares/auth";
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
 
+import validateUserStore from "./app/validators/UserStore";
+import validateUserUpdate from "./app/validators/UserUpdate";
+
 const routes = new Router();
 
-routes.post("/users", UserController.store);
+routes.post("/users", validateUserStore, UserController.store);
 routes.post("/sessions", SessionController.store);
 
-routes.put("/users", auth, UserController.update);
+routes.put("/users", auth, validateUserUpdate, UserController.update);
 
 export default routes;
